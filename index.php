d8fa0a36363dffcd04fb9d1d14d5995be490928d
Enricho Alkalas <20742166+enrichoalkalas01@users.noreply.github.com>
2019-02-04 11:15:34 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Menu</title>
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+</head>
+<body>
+	<header>
+		<nav>
+			<ul>
+				<li>
+					<a href="index.php">Home</a>
+				</li>
+				<li>
+					<a href="index.php">EMP Services</a>
+				</li>
+				<li>
+					<a href="index.php">Portofolio</a>
+				</li>
+				<li>
+					<a href="index.php">About</a>
+				</li>
+			</ul>
+		</nav>
+	</header>
+</body>
+</html>
